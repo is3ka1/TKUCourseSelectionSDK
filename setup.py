86a44fedb0848bf43a05c9fd2364d84470f42562
isekai <brian1689878638@gmail.com>
2020-08-03 18:03:38 +0800

    Ver 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="TKUCourseSelectionSDK",
-    version="0.0.1",
+    version="0.0.2",
     author="isekai",
     description="Tools for auto selecting course of TKU",
     url="https://github.com/Isekai-Seikatsu/TKUCourseSelectionSDK",
