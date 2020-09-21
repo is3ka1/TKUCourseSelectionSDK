027900db9cb0cac5e29da91454ba4c25f702c1aa
isekai <brian1689878638@gmail.com>
2020-09-21 12:05:49 +0800

    Fix error catch

--- adding_script.py
+++ adding_script.py
@@ -11,17 +11,21 @@ from data_parsing import parse_data
 @click.command()
 @click.option('--studid', prompt='Student ID', envvar='STUDID')
 @click.option('--password', prompt=True, hide_input=True, envvar='PASSWD')
-@click.argument('script', type=click.File('r'))
+@click.argument('script', type=click.File('r'), default='course_list.txt')
 def cli(studid, password, script):
     while True:
         try:
             course_selector = TKUCourseSelector()
             course_selector.login(studid, password)
+            sequence_add(script)
         except AssertionError as error:
             click.echo(f'[{datetime.now().isoformat()}] {error}')
 
-    command_pattern = re.compile('(?P<operation>[+-]{1}) *(?P<course_id>\d{4})')
 
+command_pattern = re.compile('(?P<operation>[+-]{1}) *(?P<course_id>\d{4})')
+
+
+def sequence_add(script):
     for command in script:
         match = command_pattern.match(command)
         if match:
@@ -33,15 +37,14 @@ def cli(studid, password, script):
             elif operation == '-':
                 func = course_selector.del_course
 
-            try:
-                resp = func(course_id)
-            except AssertionError as error:
-                click.echo(f'[{datetime.now().isoformat()}] {error}')
+            resp = func(course_id)
 
-            click.echo(f"course_id: {course_id}\nmsg: {parse_data(resp.text)['msg']}\n")
+            click.echo(
+                f"course_id: {course_id}\nmsg: {parse_data(resp.text)['msg']}\n")
 
         else:
             click.echo('Invalid input\n')
+
 
 if __name__ == "__main__":
     cli()
